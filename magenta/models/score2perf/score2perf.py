1fae0e3efacaca245d5eba12195a74df364ed465
natty p <napeat@gmail.com>
2019-09-19 13:14:10 -0500

    Update score2perf.py

--- magenta/models/score2perf/score2perf.py
+++ magenta/models/score2perf/score2perf.py
@@ -44,9 +44,9 @@ MAX_PITCH = 108
 
 # pylint: disable=line-too-long
 MAESTRO_TFRECORD_PATHS = {
-    'train': 'gs://magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0_train.tfrecord',
-    'dev': 'gs://magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0_validation.tfrecord',
-    'test': 'gs://magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0_test.tfrecord'
+    'train': 'gs://touhoudataset/train.tfrecord',
+    'dev': 'gs://touhoudataset/valid.tfrecord',
+    'test': 'gs://touhoudataset/test.tfrecord'
 }
 # pylint: enable=line-too-long
 
